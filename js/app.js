875cb570f84014cccb6200e10851ac8cf950e2bd
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-02-03 20:52:45 -0500

    added routes and controllers for new pages

--- js/app.js
+++ js/app.js
@@ -12,5 +12,9 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/default', {templateUrl: 'partials/default.html', controller: 'defaultCtrl'});
   $routeProvider.when('/logout', {templateUrl: 'partials/logout.html', controller: 'logoutCtrl'});
+  $routeProvider.when('/admin_home', {templateUrl: 'partials/admin_home.html', controller: 'adminHomeCtrl'});
+  $routeProvider.when('/user_home', {templateUrl: 'partials/user_home.html', controller: 'userHomeCtrl'});
+  $routeProvider.when('/admin_users', {templateUrl: 'partials/admin_users.html', controller: 'adminUsersCtrl'});
+  $routeProvider.when('/users_quizzes', {templateUrl: 'partials/users_quizzes.html', controller: 'usersQuizzesCtrl'});
   $routeProvider.otherwise({redirectTo: '/default'});
 }]);
--- js/controllers.js
+++ js/controllers.js
@@ -6,3 +6,18 @@ angular.module('myApp.controllers', []).
   controller('defaultCtrl', [function() {
 
   }]);
+  controller('logoutCtrl', [function() {
+
+  }]);
+  controller('adminHomeCtrl', [function() {
+
+  }]);
+  controller('userHomeCtrl', [function() {
+
+  }]);
+  controller('adminUsersCtrl', [function() {
+
+  }]);
+  controller('userQuizzesCtrl', [function() {
+
+  }]);
